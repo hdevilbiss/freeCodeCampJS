648f6fd9f915578d9b6ce7e21cff915cd0af2444
Hannah <45151652+shed-00@users.noreply.github.com>
2020-11-28 17:40:41 -0500

    Solve sum of nums using recursion
    Use destructuring on the input array to get the first and second values, except make sure to declare a default array just
    in case someone passes undefined into your function. Get the increment value by checking whether the difference between the first and second numbers is less than 0, setting the increment to 1 or -1 accordingly. Finally, checking whether the first number is not equal to the last number; when false, meaning equality, then pass the first value.

--- js/sum-array-of-nums.js
+++ js/sum-array-of-nums.js
@@ -1,32 +1,15 @@
 /**
  * sumAll
- * @param {Array} array of 2 nums, default empty array
- * @return {Number} sum of all nums in between 2 nums
+ * @param {Array} two vals to make a range
+ * @return {Number} sum of range
  */
-const sumAll = (arr = []) => {
+const sumAll = ([ first, last ] = [0,0]) => {
+  const increment = first - last < 0 ? 1 : -1;
+  return first !== last
+    ? first + sumAll([ first + increment, last ])
+    : first;
+ };
 
-  /**
-   * Add two numbers together
-   * To be used as param with Array.reduce
-   * @param {Number} accSum 
-   * @param {Number} curNum 
-   */
-  const reducer = (accSum = 0,curNum) => accSum + curNum;
-  
-  let myArr = arr.map(num => num).sort((a,b) => a - b);
-  let lowerNum = myArr.shift();
-  let higherNum = myArr.pop();
-  for (let i = lowerNum; i <= higherNum; i++) {
-    myArr.push(i);
-  }
-  let mySum = myArr.reduce(reducer,0);
-  
-  
-
-  
-  return mySum;
-};
-  
 /**
  * Tests for sumAll
 */
@@ -37,4 +20,3 @@ let unsortedArr = [10,5];//45
 console.log(sumAll(sortedArr));
 console.log(sumAll(undefArr));
 console.log(sumAll(unsortedArr));
-  
