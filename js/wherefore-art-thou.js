a384a545ab7401db4b299d2b7f373a2a44c61169
Hannah <45151652+shed-00@users.noreply.github.com>
2020-11-30 11:28:25 -0500

    Solve using forEach and for loop
    Only using console messages at the moment. Need to return the matching object.

--- js/wherefore-art-thou.js
+++ js/wherefore-art-thou.js
@@ -1,16 +1,29 @@
-function whatIsInAName(collection, source) {
-    var arr = [];
-    // Only change code below this line
-  
-  
-    // Only change code above this line
-    return arr;
-  }
+/**
+ * whatIsInAName
+ * @param {Array} collection of objects
+ * @param {Object} source object to find in collection
+ * @return {Object} the object in which source was found
+ */
+const whatIsInAName = (collection,source) => {
+    
+    collection.forEach(obj => {
+        for (let key in obj) {
+            //console.log(`${key}: ${obj[key]}`);
+            if (source[key] === obj[key]) {
+                console.log(`Match! ${key}: ${obj[key]}`);
+                console.dir(obj);
+            }
+            else {
+                console.log(`No match!`);
+            }
+        }
+    });
+};
   
 /**
  * Tests for whatIsInAName
  */
 let arrOfNames = [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }];
 
-console.log(whatIsInAName(arrOfNames, { last: "Capulet" }));
+console.log(whatIsInAName(arrOfNames, { last: "Capulet" }));//[{ first: "Tybalt", last: "Capulet" }]
   
