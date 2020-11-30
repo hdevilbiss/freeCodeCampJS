10018b47e700404cbf87dcc6014de9060dc89207
Hannah <45151652+shed-00@users.noreply.github.com>
2020-11-29 19:47:02 -0500

    Add filter and for loop
    Use filter method to loop through arr. Next, loop through all indices of the arguments object. Check whether the arr value is equal to the current argument.

--- js/destroyer.js
+++ js/destroyer.js
@@ -4,15 +4,19 @@
  * @return {Array} arr but minus vals from arguments object
  */
 function destroyer(arr) {
-    for (let i = 0; i < arguments.length; i++) {
-        console.log(`arguments at i = ${i}: ${arguments[i]}`);
-    }
-    return arr;
+    arr.filter(val => {
+        for (let i = 1; i < arguments.length; i++) {
+            val !== arguments[i]
+                ? console.log(`true, ${val} !== ${arguments[i]}`)
+                : console.log(`false, ${val} == ${arguments[i]}`);
+        }
+    });
+    console.log(arr);
 }
 
 /**
  * Tests for destroyer
  */
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
-destroyer(['cat','bat','mat','tat','what'],'what');
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);//[1,1]
+destroyer(['cat','bat','mat','tat','what'],'what');//['cat','bat','mat','tat']
   
