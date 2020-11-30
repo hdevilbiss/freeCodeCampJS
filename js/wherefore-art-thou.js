006f7becdcc5822798bbd354a80e74ef4e931107
Hannah <45151652+shed-00@users.noreply.github.com>
2020-11-30 10:59:00 -0500

    Create wherefore-art-thou.js

---
+++ js/wherefore-art-thou.js
@@ -0,0 +1,16 @@
+function whatIsInAName(collection, source) {
+    var arr = [];
+    // Only change code below this line
+  
+  
+    // Only change code above this line
+    return arr;
+  }
+  
+/**
+ * Tests for whatIsInAName
+ */
+let arrOfNames = [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }];
+
+console.log(whatIsInAName(arrOfNames, { last: "Capulet" }));
+  
