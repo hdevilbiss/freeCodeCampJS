a4bf574b675843af83f3249a48ed15195f6b85da
Hannah <45151652+shed-00@users.noreply.github.com>
2020-12-02 18:29:54 -0500

    ✅ Add pass/fail testing for whatIsInAName

--- js/wherefore-art-thou.js
+++ js/wherefore-art-thou.js
@@ -2,28 +2,64 @@
  * whatIsInAName
  * @param {Array} collection of objects
  * @param {Object} source object to find in collection
- * @return {Object} the object in which source was found
+ * @return {Array} array of matching objects
  */
 const whatIsInAName = (collection,source) => {
-    
+    let newArr = [];
     collection.forEach(obj => {
         for (let key in obj) {
-            //console.log(`${key}: ${obj[key]}`);
-            if (source[key] === obj[key]) {
-                console.log(`Match! ${key}: ${obj[key]}`);
-                console.dir(obj);
-            }
-            else {
-                console.log(`No match!`);
+            if ( source[key] === obj[key] ) {
+                newArr.push(obj)
             }
         }
     });
+    return newArr;
 };
   
 /**
  * Tests for whatIsInAName
  */
-let arrOfNames = [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }];
+const arrOfNames = [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }];
+const arrOfSnacks = [{ "apple": 1, "banana": 2 }, { "banana": 2 }, { "apple": 1, "banana": 2, "cookie": 2 }];
 
-console.log(whatIsInAName(arrOfNames, { last: "Capulet" }));//[{ first: "Tybalt", last: "Capulet" }]
+let nameTest = whatIsInAName(arrOfNames, { last: "Capulet" });
+let snackTest = whatIsInAName(arrOfSnacks, { "apple": 1, "banana": 2 });
+
+let nameRes = [{ first: "Tybalt", last: "Capulet" }];
+let snackRes = [{ "apple": 1, "banana": 2 }, { "apple": 1, "banana": 2, "cookie": 2 }];
+
+/**
+ * Actual results
+ */
+console.log(nameTest);
+console.log(snackTest);
+
+/**
+ * checker
+ * @param {Array} test actual test result
+ * @param {Array} result expected test result
+ * @return {String} pass or fail
+ */
+ const checker = (test,result) => {
+    if (test.length !== result.length) {
+        return "FAIL";
+    }
+
+    let errors = [];
+    
+    test.forEach((val,idx) => {
+        for (let key in val) {
+            if (!result[idx].hasOwnProperty(key) || !result[idx][key] === val[key]) {
+                errors.push("f");
+            }
+        }
+    });
+
+    return errors.length === 0
+        ? "PASS"
+        : "FAIL";
+ }
+
+console.log(checker(nameTest,nameRes))
+console.log(checker(snackTest,snackRes))
   
