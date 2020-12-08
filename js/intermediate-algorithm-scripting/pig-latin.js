3d9768a27e7be14c7f0c887b81b0d28043354dd1
Hannah <45151652+shed-00@users.noreply.github.com>
2020-12-08 11:57:43 -0500

    🚧 Solve pig-latin, but probably with lots of redundancy

--- js/intermediate-algorithm-scripting/pig-latin.js
+++ js/intermediate-algorithm-scripting/pig-latin.js
@@ -4,18 +4,39 @@
  * @return {String} str but pig latin
  */
 const translatePigLatin = (str) => {
-    let vowelRegex = /^[aeiouy]/;
+    let startsWithVowelRegex = /^[aeiouy]/;
+    let startsWithNotAVowelRegex = /^[^aeiouy]{1,}/;
+    let justVowelsRegex = /[aeiou]/;
     let newStr = '';
     
-    if ( vowelRegex.test(str) === true ) {
+    if ( startsWithVowelRegex.test(str) === true ) {
         newStr = str.concat('way')
     }
     else {
-        let firstLetter = str.slice(0,1);
-        newStr = newStr.concat(str.substring(1),firstLetter,'ay');
+        let strOfStartingConsonants = '';
+        let startingConsonantLength = 0;
+        let strAsArray = str.split('');
+        let arrOfVowels = [];
+
+        strOfStartingConsonants = str.match(startsWithNotAVowelRegex)[0];
+        startingConsonantLength = strOfStartingConsonants.length;
+        console.log(`strOfStartingConsonants:${strOfStartingConsonants}`)
+
+        strAsArray.forEach(letter => {
+            if (justVowelsRegex.test(letter) === true) arrOfVowels.push(letter)
+        });
+
+        if (arrOfVowels.length === 0) {
+            newStr = str.concat('ay')
+        }
+        else {
+            newStr = newStr.concat(str.substring(startingConsonantLength),strOfStartingConsonants,'ay');
+        }
     }
-    console.log(`newStr:${newStr}`);
+    
     console.log(`str:${str}`);
+    console.log(`newStr:${newStr}`);
+    
     return newStr;
 }
 
@@ -24,3 +45,4 @@ console.log(translatePigLatin("california") === "aliforniacay")
 console.log(translatePigLatin("algorithm") === "algorithmway")
 console.log(translatePigLatin("glove") === "oveglay")
 console.log(translatePigLatin("schwartz") === "artzschway")
+console.log(translatePigLatin("rhythm") === "rhythmay")
