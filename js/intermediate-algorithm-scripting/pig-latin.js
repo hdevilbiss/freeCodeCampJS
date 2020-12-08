beb7d682e69531b23e5eaf8b35c46df9e3c55506
Hannah <45151652+shed-00@users.noreply.github.com>
2020-12-08 11:23:36 -0500

    🚧 Solve basic pig latin, but fail with more complicated consonant groups

--- js/intermediate-algorithm-scripting/pig-latin.js
+++ js/intermediate-algorithm-scripting/pig-latin.js
@@ -5,18 +5,22 @@
  */
 const translatePigLatin = (str) => {
     let vowelRegex = /^[aeiouy]/;
-
     let newStr = '';
-
     
-
-    vowelRegex.test(str) === true
-        ? newStr = str.concat('way')
-        : newStr = str.concat(str,'ay');
-
-    console.log(`newStr:${newStr}`)
+    if ( vowelRegex.test(str) === true ) {
+        newStr = str.concat('way')
+    }
+    else {
+        let firstLetter = str.slice(0,1);
+        newStr = newStr.concat(str.substring(1),firstLetter,'ay');
+    }
+    console.log(`newStr:${newStr}`);
+    console.log(`str:${str}`);
+    return newStr;
 }
 
 console.log(translatePigLatin("consonant") === "onsonantcay")
 console.log(translatePigLatin("california") === "aliforniacay")
 console.log(translatePigLatin("algorithm") === "algorithmway")
+console.log(translatePigLatin("glove") === "oveglay")
+console.log(translatePigLatin("schwartz") === "artzschway")
