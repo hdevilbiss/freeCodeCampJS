20578fd03af95ab09ca1c6852a53ad5e848af2bb
Hannah <45151652+shed-00@users.noreply.github.com>
2020-12-15 11:59:34 -0500

    🚧 Start search-and-replace

--- js/intermediate-algorithm-scripting/search-and-replace.js
+++ js/intermediate-algorithm-scripting/search-and-replace.js
@@ -5,8 +5,17 @@
  * @param {String} after: Replacement
  * @return {String}
  */
-const myReplace = (str) => (before) => (after) => {
-
+const myReplace = (str,before,after) => {
+    const beforeRegex = new RegExp(before,"g");
+    let result = str.match(beforeRegex);
+    const uppercaseRegex = /[A-Z]/;
+    if (uppercaseRegex.test(result[0][0]) == true && uppercaseRegex.test(after[0]) == false) {
+        after[0].toUpperCase
+    }
+    console.log(result,after,after[0].toUpperCase)
+    console.log(uppercaseRegex.test(result[0][0]))
 }
-  
-myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+
+myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped")
+myReplace("Let us get back to more Coding", "Coding", "algorithms")
+myReplace("His name is Tom", "Tom", "john")
