68f147e6489caa81c2cee69a5a85db6b934dbd4c
Hannah <45151652+shed-00@users.noreply.github.com>
2020-12-16 13:46:30 -0500

    ✅ ♻️ Solve search-and-replace with 5 lines. Note: I love the ternary operator omg

--- js/intermediate-algorithm-scripting/search-and-replace.js
+++ js/intermediate-algorithm-scripting/search-and-replace.js
@@ -2,21 +2,18 @@
  * myReplace
  * @param {String} str : Target for search
  * @param {String} before: Criterion for search
- * @param {String} after: Replacement
+ * @param {String} after: Replacement (first letter case must match 'before')
  * @return {String}
  */
 const myReplace = (str,before,after) => {
-    const searchRegex = new RegExp(before, 'g');
-
-    let match = str.match(searchRegex);
     let replacement = after.split("");
-
-    if (/[A-Z]/.test(match[0].charAt(0)) === true && /[A-Z]/.test(after[0]) === false) {
-        replacement[0] = replacement[0].toUpperCase();
-    }
+    /[A-Z]/.test(before.charAt(0)) === true
+        ? replacement[0] = replacement[0].toUpperCase()
+        : replacement[0] = replacement[0].toLowerCase();
     return str.replace(before,replacement.join(""));
 }
 
 console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"))
 console.log(myReplace("Let us get back to more Coding", "Coding", "algorithms"))
 console.log(myReplace("His name is Tom", "Tom", "john"))
+console.log(myReplace("I think we should look up there", "up", "Down"))
