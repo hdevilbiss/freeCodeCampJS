640b3bac689776e46118911913bedef912bfb280
Hannah <45151652+shed-00@users.noreply.github.com>
2020-12-16 13:32:57 -0500

    ♻️  🚧 Solve search-and-replace less messily, but still room for improvement. Use a function as second argument to replace?

--- js/intermediate-algorithm-scripting/search-and-replace.js
+++ js/intermediate-algorithm-scripting/search-and-replace.js
@@ -9,23 +9,14 @@ const myReplace = (str,before,after) => {
     const searchRegex = new RegExp(before, 'g');
 
     let match = str.match(searchRegex);
-
-    let newAfter = after.split("");
-
-    console.log(newAfter);
+    let replacement = after.split("");
 
     if (/[A-Z]/.test(match[0].charAt(0)) === true && /[A-Z]/.test(after[0]) === false) {
-        newAfter[0] = newAfter[0].toUpperCase();
+        replacement[0] = replacement[0].toUpperCase();
     }
-
-    newAfter = newAfter.join("");
-    console.log(newAfter);
-
-    let newStr = str.replace(before,newAfter);
-
-    console.log(newStr);
+    return str.replace(before,replacement.join(""));
 }
 
-myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped")
-myReplace("Let us get back to more Coding", "Coding", "algorithms")
-myReplace("His name is Tom", "Tom", "john")
+console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"))
+console.log(myReplace("Let us get back to more Coding", "Coding", "algorithms"))
+console.log(myReplace("His name is Tom", "Tom", "john"))
