1eae8267587792eff0ad37c8528f9ca93a7d2388
Hannah <45151652+shed-00@users.noreply.github.com>
2020-12-24 13:05:29 -0500

    💡 Reduce block comments to line comments and remove whitespace

--- js/intermediate-algorithm-scripting/fibonacci-sum.js
+++ js/intermediate-algorithm-scripting/fibonacci-sum.js
@@ -5,28 +5,17 @@
  */
 const sumFibs = (num) => {
   const fibonacci = [0,1,1];
-  /**
-   * Get the full Fibonacci sequence as an array
-   */
+  // Get the full Fibonacci sequence as an array
   while ( fibonacci[fibonacci.length - 1] < num ) {
     // Push the sum of the second-to-last and the last numbers in the array, onto the array
-    fibonacci.push(fibonacci[fibonacci.length - 2] + fibonacci[fibonacci.length - 1]);
+    fibonacci.push( fibonacci[fibonacci.length - 2] + fibonacci[fibonacci.length - 1] );
   }
-  console.log(fibonacci)
-
   /**
    * Filter the Fibonacci array by num % 2 == 1 (keep only odds)
    * Reduce the new array into one value: its sum
    */
   return fibonacci.filter(fibNum => (fibNum % 2 === 1) && (fibNum <= num)).reduce(((acc,curVal) => acc + curVal), 0);
 }
-
-/**
- * The first six numbers of the Fibonacci sequence are
- * 0, 1, 1, 2, 3, 5 and 8 ... 13, 21, 34, ...
- * After 0 and 1, it is the sum of the previous 2 nums
- * Fn = 0 + 1 + (F1 + F0) + (F2 + F1) + (F3 + F2) + (F4 + F3) + ... (Fn-1 + Fn-2)
- */
 
 /**
  * Tests for sumFibs
