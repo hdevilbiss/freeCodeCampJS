b027ad2886364ac43a7b586c0289307d423a2251
Hannah <45151652+shed-00@users.noreply.github.com>
2020-12-29 15:45:50 -0500

    ✅ Solve smallest-common-multiple using Euclidean Algorithm inside reduce

--- js/intermediate-algorithm-scripting/smallest-common-multiple.js
+++ js/intermediate-algorithm-scripting/smallest-common-multiple.js
@@ -7,44 +7,47 @@
  * @return {Number} least common multiple also divisible by all nums in range
  */
 const smallestCommons = (arr) => {
-  const myArr = arr.sort();
-  const a = myArr[0];
-  const b = myArr[1];
-  const LCM = [];
+  const myArr = arr.sort((a, b) => a - b);
+  const minNum = myArr[0];
+  const maxNum = myArr[1];
+  const rangeOfNums = [];
 
   /**
-   * getPrimes
-   * @param {Number} num
-   * @return {Array} arr of prime numbers <= num
+   * GCD: Greatest Common Denominator
+   * 1st run: num1 = lcm, num2 = a number
+   * Check if num2 === 0
+    * if T return lcm
+    * if F
+      * 2nd run: num1 = a number, num2 = the remainder of lcm / num2
+      * Check if the smallest common multiple is cleanly divisible by the number
+      * if T return y
+      * if F
+        * 3rd run: Check if the remainder of (x / y) / y
+        * ************************************************************************************************
+   * @param {Number} x
+   * @param {Number} y
    */
-  const getPrimes = (num) => {
-    let arrOfNums = Array.from({length: num + 1}, (v, k) => k).slice(2);
-    let onlyPrimes = arrOfNums.filter(n => {
-      let m = n - 1;
-      while (m > 1 && m >= Math.sqrt(n)) {
-        if (n % m === 0) return false;
-        m--;
-      }
-      return true;
-    });
-    return onlyPrimes;
-  }
+  const GCD = (num1, num2) => {
+    // Euclidean Algorithm?
+    return num2 === 0
+      ? num1
+      : GCD(num2, num1 % num2);
+  };
 
-  const aPrimes = getPrimes(a);
-  const bPrimes = getPrimes(b);
+  // Get array of range [a:b]
+  for (let i = minNum; i <= maxNum; i++) {
+    rangeOfNums.push(i);
+  }
 
   /**
-  * LCM is a product of:
-  * Common primes (duplicated in a and b)
-  * Unique primes
-  */
-
-  // Get b primes that a also has
-  for (let i = 0; i < bPrimes.length; i++) {
-    if (aPrimes.includes(bPrimes[i])) {
-      LCM.push(bPrimes[i]);
-    }
-  }
+   * Reduce the range
+   * the product of the smallest multiple and a number
+   * divided by their greatest common denominator
+   * @link https://developer.mozilla.org/en-us/docs/web/javascript/reference/global_objects/array/reduce
+   */
+  return rangeOfNums.reduce((lcm,num) => {
+    return (lcm * num) / GCD(lcm,num);
+  });
 }
 
 /**
@@ -55,8 +58,8 @@ const testArr = [];
 testArr.push(
   [[1,5], 60],
   [[5,1], 60],
-  [[1,3], 3],
-  [[3,1], 3],
+  [[1,3], 6],
+  [[3,1], 6],
   [[2,3], 6],
   [[3,2], 6],
   [[2,10],2520],
@@ -65,5 +68,5 @@ testArr.push(
 );
 
 testArr.forEach(([arr, expectedResult]) => {
-  console.log(smallestCommons(arr) === expectedResult ? "PASS" : "FAIL")
+  console.log(smallestCommons(arr) === expectedResult ? `PASS: ${smallestCommons(arr)} === ${expectedResult} with ${arr}` : `FAIL: Got ${smallestCommons(arr)} with ${arr}, expected ${expectedResult}`)
 });
