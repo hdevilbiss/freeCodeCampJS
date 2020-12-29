9b6a5bfda953e10cde69ae8ae914f74bcb5e4728
Hannah <45151652+shed-00@users.noreply.github.com>
2020-12-29 16:06:09 -0500

    💡 Remove comments from smallest-common-multiple

--- js/intermediate-algorithm-scripting/smallest-common-multiple.js
+++ js/intermediate-algorithm-scripting/smallest-common-multiple.js
@@ -1,8 +1,5 @@
 /**
  * smallestCommons
- * "The LCM is the smallest positive integer
- * that is evenly divisible by both a and b"
- * But we also need to consider each num in between a and b
  * @param {Array} arr with 2 nums
  * @return {Number} least common multiple also divisible by all nums in range
  */
@@ -12,26 +9,11 @@ const smallestCommons = (arr) => {
   const maxNum = myArr[1];
   const rangeOfNums = [];
 
-  /**
-   * GCD: Greatest Common Denominator
-   * 1st run: num1 = lcm, num2 = a number
-   * Check if num2 === 0
-    * if T return lcm
-    * if F
-      * 2nd run: num1 = a number, num2 = the remainder of lcm / num2
-      * Check if the smallest common multiple is cleanly divisible by the number
-      * if T return y
-      * if F
-        * 3rd run: Check if the remainder of (x / y) / y
-        * ************************************************************************************************
-   * @param {Number} x
-   * @param {Number} y
-   */
-  const GCD = (num1, num2) => {
-    // Euclidean Algorithm?
-    return num2 === 0
-      ? num1
-      : GCD(num2, num1 % num2);
+  // Euclidean Algorithm
+  const GCD = (x, y) => {
+    return y === 0
+      ? x
+      : GCD(y, x % y);
   };
 
   // Get array of range [a:b]
@@ -40,9 +22,7 @@ const smallestCommons = (arr) => {
   }
 
   /**
-   * Reduce the range
-   * the product of the smallest multiple and a number
-   * divided by their greatest common denominator
+   * Reduce the range to 1 LCM
    * @link https://developer.mozilla.org/en-us/docs/web/javascript/reference/global_objects/array/reduce
    */
   return rangeOfNums.reduce((lcm,num) => {
