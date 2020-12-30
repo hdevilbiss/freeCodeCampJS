90cbbd923b8b5824166254657496c4e10f3bb75e
Hannah <45151652+shed-00@users.noreply.github.com>
2020-12-30 13:00:04 -0500

    ✅ Solve dropElements using findIndex and filter array methods

--- js/intermediate-algorithm-scripting/drop-it.js
+++ js/intermediate-algorithm-scripting/drop-it.js
@@ -5,7 +5,10 @@
  * @return {Array} filtered arr
  */
 const dropElements = (arr,func) => {
-  return arr.filter(func);
+  const firstTrueIdx = arr.findIndex(func);
+  return firstTrueIdx === -1
+    ? []
+    : arr.filter((val,idx) => idx >= firstTrueIdx);
 }
 
 /**
@@ -14,11 +17,12 @@ const dropElements = (arr,func) => {
 const testArr = [];
 
 testArr.push(
-  [[[1, 2, 3], n => n < 3], [1,2]],
-  [[[-1,0,2,3,-9], n => n >= 0], [0,2,3]],
+  [[[1, 2, 3], n => n < 3], [1,2,3]],
+  [[[-1,0,2,3,-9], n => n >= 0], [0,2,3,-9]],
   [[[1, 2, 3, 4], function(n) {return n >= 3;}],[3, 4]],
   [[[1, 2, 3, 9, 2], function(n) {return n > 2;}],[3, 9, 2]],
-  [[[0, 1, 0, 1], function(n) {return n === 1;}],[1, 0, 1]]
+  [[[0, 1, 0, 1], function(n) {return n === 1;}],[1, 0, 1]],
+  [[[1, 2, 3, 4], function(n) {return n > 5;}],[]],
 );
 
 testArr.forEach(([[arr,func], expectedResult]) => {
