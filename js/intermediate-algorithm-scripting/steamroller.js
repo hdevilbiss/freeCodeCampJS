6243ae5b2f096c103304c49c3a3a9b9391167915
Hannah <45151652+shed-00@users.noreply.github.com>
2020-12-31 12:35:45 -0500

    ✅ steamroller: Solve with `reduce` & recursion. The accumulator is an empty array; for each element, check whether it is an array; if NOT, then add to the array; otherwise, concatenate a recursive call to steamrollArray, except providing the current element as the argument.

--- js/intermediate-algorithm-scripting/steamroller.js
+++ js/intermediate-algorithm-scripting/steamroller.js
@@ -3,11 +3,20 @@
  * Flatten a nested array no matter how wonky
  * @param {Array} arr which may have sub arrays
  * @return {Array} one flattened array
+ *
+ * The accumulator is an empty array; for each element, check whether it is an array;
+ * if NOT, then add to the array;
+ * otherwise, concatenate a recursive call to steamrollArray,
+ * except providing the current element as the argument.
+ *
  */
 const steamrollArray = (arr) => {
-  let newArr = arr.flat();
-  return newArr;
+  return arr.reduce(((accumulator,current) => {
+    return !Array.isArray(current)
+      ? accumulator.concat(current)
+      : accumulator.concat(steamrollArray(current));
+  }),[]);
 }
 
-console.log(steamrollArray([[["a"]], [["b"]]]))
-console.log(steamrollArray([1, [2], [3, [[4]]]]))
+console.log(steamrollArray([[["a"]], [["b"]]])); // expected [a,b]
+console.log(steamrollArray([1, [2], [3, [[4]]]])) // expected [1,2,3,4]
