7fad3330da72f29d79ebdb1afe05df1dab24d08a
Hannah <45151652+shed-00@users.noreply.github.com>
2020-12-31 12:36:11 -0500

    🔥 💡 Remove comments from steamrollArray

--- js/intermediate-algorithm-scripting/steamroller.js
+++ js/intermediate-algorithm-scripting/steamroller.js
@@ -3,12 +3,6 @@
  * Flatten a nested array no matter how wonky
  * @param {Array} arr which may have sub arrays
  * @return {Array} one flattened array
- *
- * The accumulator is an empty array; for each element, check whether it is an array;
- * if NOT, then add to the array;
- * otherwise, concatenate a recursive call to steamrollArray,
- * except providing the current element as the argument.
- *
  */
 const steamrollArray = (arr) => {
   return arr.reduce(((accumulator,current) => {
