822def35ecd8067e952d70b0a5082ed90fd5f256
Hannah <45151652+shed-00@users.noreply.github.com>
2021-01-04 13:02:15 -0500

    ✅ everything-be-true: Solve checking the truthiness of each value in a collection against a predicate

---
+++ js/intermediate-algorithm-scripting/everything-be-true.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * @param {Array} collection
+ * @param {String} pre
+ * Check if the predicate is truthy on all elements of a collection
+ */
+const truthCheck = (collection, pre) => {
+  let flag = true;
+  if (pre) {
+    collection.forEach(obj => {
+      if (!obj[pre]) flag = false;
+    });
+  }
+  console.log(flag);
+  return flag;
+}
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")); // true
+
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")); // false
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age")); // false
+
+console.log(truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastForward", "onBoat": null}], "onBoat")); // false
+
+console.log(truthCheck([{"single": "double"}, {"single": NaN}], "single"))//false
