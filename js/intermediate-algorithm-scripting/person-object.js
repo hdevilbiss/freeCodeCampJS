88e9998958a8c697c16698b41fc53173a0cd4cd6
Hannah <45151652+shed-00@users.noreply.github.com>
2021-01-11 14:44:52 -0500

    ✅ int algorithms for person-object: Get expected results in console

--- js/intermediate-algorithm-scripting/person-object.js
+++ js/intermediate-algorithm-scripting/person-object.js
@@ -1,39 +1,46 @@
-// export default class Person {
-//   constructor(firstAndLast) {
-//     this.firstAndLast = firstAndLast;
-//   }
-//   getFullName = () => {
-//     return this.firstAndLast;
-//   }
-// }
-
+/**
+ *
+ * @param {String} firstAndLast
+ * A Person function
+ */
 var Person = function(firstAndLast) {
-  constructor = () => {
-    this.fullName = firstAndLast;
-    this.firstName = firstAndLast.split(" ")[0];
-    this.lastName = firstAndLast.split(" ")[1];
-  };
-  this.getFullName = () => {
-    return this.fullName;
-  };
-  this.getFirstName = () => {
-    return this.firstName;
-  };
-  this.getLastName = () => {
-    return this.lastName;
-  };
-  this.setFullName = () => {
-    this.fullName = firstAndLast;
-  };
-  this.setFirstName = () => {
-    this.firstName = firstAndLast.split(" ")[0];
-  };
-  this.setLastName = () => {
-    this.lastName = firstAndLast.split(" ")[1];
-  };
+  let fullName = firstAndLast;
+  let firstName = fullName.split(" ")[0];
+  let lastName = fullName.split(" ")[1];
+
+  this.getFirstName = () => firstName;
+  this.getLastName = () => lastName;
+  this.getFullName = () => fullName;
+  this.setFirstName = (name) => {
+    firstName = name;
+    this.setFullName(firstName + " " + lastName);
+  }
+  this.setLastName = (name) => {
+    lastName = name;
+    this.setFullName(firstName + " " + lastName);
+  }
+  this.setFullName = (name) => {
+    fullName = name;
+    let arr = fullName.split(" ");
+    lastName = arr.pop();
+    firstName = arr.join(" ");
+  }
 };
 
+// Person.prototype.getFullName = () => this.fullName;
+// Person.prototype.getFirstName = () => this.firstName;
+// Person.prototype.getLastName = () => this.lastName;
+
 var bob = new Person('Bob Ross');
-console.log(bob.getFullName())
-console.log(bob.getFirstName())
-console.log(bob.getLastName())
+console.log(bob.getFullName()); //Bob Ross
+console.log(bob.getFirstName()); //Bob
+console.log(bob.getLastName()); //Ross
+bob.setFirstName("Lloyd");
+console.log(bob.getFirstName()); //Lloyd
+console.log(bob.getFullName()); //Lloyd Ross
+bob.setFullName("Bob Albus DeVil");
+console.log(bob.getFirstName()); // Bob Albus
+console.log(bob.getFullName()); //expected Bob Albus DeVil
+console.log(bob.getLastName()); //DeVil
+bob.setLastName("Ross");
+console.log(bob.getFullName()); // Bob Albus Ross
