f67c485106426be56b77e45553bb8406f65ecf08
Hannah <45151652+shed-00@users.noreply.github.com>
2021-01-11 15:09:00 -0500

    ✅ test: Finish person-object unit tests - my first unit test! 😄 🥳

--- test/person-object.spec.js
+++ test/person-object.spec.js
@@ -2,16 +2,42 @@ const assert = require("assert");
 const Person = require("../js/intermediate-algorithm-scripting/person-object");
 
 describe('Person class', () => {
-  var InstanceOfPerson;
+  var bob = new Person('Bob Ross');
 
-  beforeEach(() => {
-    InstanceOfPerson = new Person();
-  });
-
-  it('should have six keys when instantiated', () => {
-    assert.strictEqual(Object.keys(InstanceOfPerson).length,6)
+  it('a new Person should have six keys when instantiated', () => {
+    assert.strictEqual(Object.keys(bob).length, 6)
   });
   it('a var made from Person should be an instance of the Person object', () => {
-    assert.strictEqual(InstanceOfPerson instanceof Person, true);
+    assert.strictEqual(bob instanceof Person, true);
+  });
+  it('should return undefined if firstName var gets accessed directly', () => {
+    assert.strictEqual(bob.firstName, undefined);
+  });
+  it('should return undefined if lastName var gets accessed directly', () => {
+    assert.strictEqual(bob.lastName, undefined);
+  });
+  it('should return Bob when getFirstName is invoked', () => {
+    assert.strictEqual(bob.getFirstName(), "Bob");
+  });
+  it('should return Ross when getLastName is invoked', () => {
+    assert.strictEqual(bob.getLastName(), "Ross");
+  });
+  it('should return Bob Ross when getFullName is invoked', () => {
+    assert.strictEqual(bob.getFullName(), "Bob Ross");
+  });
+  it('getFullName should return Haskell Ross after invoking setFirstName("Haskell")', () => {
+    bob.setFirstName("Haskell");
+    assert.strictEqual(bob.getFullName(), "Haskell Ross");
+  });
+  it('getFullName should return Haskell Curry after invoking setLastName("Curry")', () => {
+    bob.setLastName("Curry");
+    assert.strictEqual(bob.getFullName(), "Haskell Curry");
+  });
+  it('getFirstName should return John after invoking setFullName("John Doe")', () => {
+    bob.setFullName("John Doe");
+    assert.strictEqual(bob.getFirstName(), "John");
+  });
+  it('getLastName should return "Doe"', () => {
+    assert.strictEqual(bob.getLastName(), "Doe");
   });
 });
